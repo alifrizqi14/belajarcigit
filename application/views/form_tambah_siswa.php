e14359094cdf33b0eade7355bfbaee1dcf0f7bb7
alifrizqi14 <alif.aqm@bsi.ac.id>
2024-03-25 11:30:01 +0700

    Form tambah siswa

---
+++ application/views/form_tambah_siswa.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <pre>
+        <form action="" method="post" enctype="multipart/form-data">
+        <label>NIM           : </label><input type="text" name="txtnim">
+        <label>Nama          : </label><input type="text" name="txtnama">
+        <label>Jenis Kelamin : </label><input type="radio" name="jk" value="laki-laki">Laki-laki <input type="radio" name="jk" value="perempuan">Perempuan
+        <label>Alamat        : </label><textarea name="txtalamat"></textarea>
+        <label>Foto          : </label><input type="file" name="foto">
+        <input type="submit" value="SIMPAN">
+        </form>
+    </pre>
+</body>
+
+</html>
